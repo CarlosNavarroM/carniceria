25b1ab3eda117fb33cbb77c8d394910b2e19c0ba
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2024-06-17 23:04:25 -0400

    update

--- carniceriaelcarvajal/frontend/views.py
+++ carniceriaelcarvajal/frontend/views.py
@@ -1,5 +1,4 @@
 # frontend/views.py
-
 from django.shortcuts import render
 
 def index(request):
@@ -8,20 +7,23 @@ def index(request):
 def productos(request):
     return render(request, 'frontend/productos.html')
 
-def carrito(request):
-    return render(request, 'frontend/carrito.html')
-
-def pago(request):
-    return render(request, 'frontend/pago.html')
-
 def contacto(request):
     return render(request, 'frontend/contacto.html')
 
 def sugerencias(request):
     return render(request, 'frontend/sugerencias.html')
 
+def login(request):
+    return render(request, 'frontend/login.html')
+
+def carrito(request):
+    return render(request, 'frontend/carrito.html')
+
+def pago(request):
+    return render(request, 'frontend/pago.html')
+
 def signup(request):
     return render(request, 'frontend/signup.html')
 
-def login(request):
-    return render(request, 'frontend/login.html')
+def navbar(request):
+    return render(request, 'frontend/navbar.html')

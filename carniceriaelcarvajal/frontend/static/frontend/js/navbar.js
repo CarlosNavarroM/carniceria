fe11f1470ce1d3ad57bb6833ce2edeef0ca480eb
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2024-06-18 19:09:10 -0400

    update

--- carniceriaelcarvajal/frontend/static/frontend/js/navbar.js
+++ carniceriaelcarvajal/frontend/static/frontend/js/navbar.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-    $("#navbar-placeholder").load("navbar.html", function() {
+    $("#navbar-placeholder").load("{% url 'navbar' %}", function() {
         updateCartCount();
+
+        // Cierra el menú colapsable al hacer clic en un enlace del menú
+        $('.navbar-nav a').click(function() {
+            if ($('.navbar-toggler').is(':visible')) {
+                $('.navbar-collapse').collapse('hide');
+            }
+        });
+
+        // Cierra el menú colapsable al hacer clic en cualquier área fuera del menú en dispositivos móviles
+        $(document).click(function(event) {
+            var clickover = $(event.target);
+            var _opened = $(".navbar-collapse").hasClass("show");
+            if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+                $(".navbar-toggler").click();
+            }
+        });
     });
 
     function updateCartCount() {
